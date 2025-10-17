c038a0c6f61f5268c48c5686e3e77464c6354d49
kevin <kevinmpz10@icloud.com>
2025-10-17 15:56:26 -0300

    📐 chore(rsvp): define DTO and data contracts

---
+++ src/features/rsvp/types.js
@@ -0,0 +1,15 @@
+/**
+ * @typedef {Object} RsvpDto
+ * @property {string} name
+ * @property {string} email
+ * @property {string} [phone]
+ * @property {number} guests
+ * @property {string} [message]
+ */
+export const RsvpDto = {
+  name: '',
+  email: '',
+  phone: '',
+  guests: 0,
+  message: '',
+}
